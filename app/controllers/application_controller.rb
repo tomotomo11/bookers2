16fb4fe3bcd888ae40d5efd7cb295e0ff0e80c1d
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-04-20 13:27:21 +0000

    [Rest3]4-27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about] # top, about の2つのアクションのみ、ログイン無しでもアクセス可能にする
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
