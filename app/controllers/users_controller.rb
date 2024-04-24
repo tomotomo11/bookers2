0107ad99085fbf8105a4c0b8a5b197da220b1b29
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-04-24 09:52:58 +0000

    [Finish]4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def edit
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(current_user.id)
     end
 
     @user = User.find(current_user.id)
