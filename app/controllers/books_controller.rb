acb39ee2054b68dcda4286198e7568121fcb48c1
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-04-24 04:56:20 +0000

    [Rest16]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,11 +23,10 @@ class BooksController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
-    unless user.id == current_user.id
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
     redirect_to books_path
     end
-    @book = Book.find(params[:id])
   end
 
   def update
